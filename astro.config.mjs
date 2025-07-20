4d6da6c3e58edd85f32d3e05609be8e5310f0cba
abdulwahab alshehri <abdul.2020alsh@gmail.com>
2025-07-20 21:21:36 +0300

    update the json and the config for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://abdul2025.github.io/astro_portfolio',
+    base: '/astro_portfolio/',
+  });
