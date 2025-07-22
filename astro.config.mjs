5a66ec99b81137c32b92404b7c50cb5669dc03f8
abdulwahab alshehri <abdul.2020alsh@gmail.com>
2025-07-23 01:01:39 +0300

    update the config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://abdul2025.github.io/astro_portfolio',
-    base: '/astro_portfolio/',
+    base: '/astro_portfolio/'
   });
