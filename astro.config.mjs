83f4af01bd8e64c580699bbfa042841df6c661b4
abdulwahab alshehri <abdul.2020alsh@gmail.com>
2025-07-23 03:15:56 +0300

    fix the config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-export default defineConfig(({ command, mode }) => {
-  const isProd = mode === 'production';
-
-  return {
-    site: isProd
-      ? 'https://abdul2025.github.io'
-      : 'http://localhost:4321',
-
-    base: isProd ? '/astro_portfolio/' : '/',
-  };
-});
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://abdul2025.github.io',
+    base: '/astro_portfolio/'
+  });
